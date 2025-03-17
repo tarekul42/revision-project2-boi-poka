771d303d130a3dc6127d27fb267674f0b6863eb0
tarekul42 <tarekulrifat205@gmail.com>
2025-03-17 07:09:24 +0600

    routing exploration done. so, other components added done on main layout

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            disum disum dashing board
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,20 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Root from "./Components/Root/Root.jsx";
 import ErrorPage from "./Components/ErrorPage/ErrorPage.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Root />} />
+        <Route path="/" element={<Root />}>
+          {/* Default page when visiting "/" */}
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+
+        {/* Error Page (Shown for invalid routes) */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
