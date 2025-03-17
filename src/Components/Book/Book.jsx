4df14aed6fafa5c43423f9fef2ca46a103486098
tarekul42 <tarekulrifat205@gmail.com>
2025-03-17 08:01:52 +0600

    books section almost done. now working for escaping from this page

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { FaRegStar } from 'react-icons/fa';
 
 const Book = ({ book }) => {
-  const { image, bookName, author } = book;
+  const { image, bookName, author, tags, category, rating } = book;
   return (
     <div className="card bg-base-100 w-96 shadow-sm p-6 ">
       <figure className="bg-blue-200 p-8 rounded-lg">
-        <img
-          src={image}
-          className="h-[166px]"
-          alt={bookName}
-        />
+        <img src={image} className="h-[166px]" alt={bookName} />
       </figure>
       <div className="card-body">
+        <div className="flex gap-4">
+          {tags.map((tag, index) => (
+            <button key={index} className="btn btn-soft btn-success">
+              {tag}
+            </button>
+          ))}
+        </div>
+
         <h2 className="card-title">
-         {bookName}
+          {bookName}
           <div className="badge badge-secondary">NEW</div>
         </h2>
-        <p>
-          By: {author}
-        </p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <p>By: {author}</p>
+        <div className="card-actions justify-between border-t-2 border-dashed pt-4 mt-2">
+          <div className="badge badge-outline">{category}</div>
+          <div className="badge">{rating}<FaRegStar/></div>
         </div>
       </div>
     </div>
