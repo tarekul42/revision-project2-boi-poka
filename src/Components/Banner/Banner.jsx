8b1f5e7d122bed7ac18874a46dde041732bab2f2
tarekul42 <tarekulrifat205@gmail.com>
2025-03-17 07:29:34 +0600

    data fetched from local file to the UI

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -11,11 +11,6 @@ const Banner = () => {
         />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
