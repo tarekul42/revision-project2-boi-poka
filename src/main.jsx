5059bdb3be9b7e34626ed902552d788716c8edf1
tarekul42 <tarekulrifat205@gmail.com>
2025-03-17 06:44:00 +0600

    errorpage working correctly from react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Root from "./Components/Root/Root.jsx";
+import ErrorPage from "./Components/ErrorPage/ErrorPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Root />
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
